1ad36e4d0fb82191e553cec2fb10aacdfe9396f6
AndreaPotenza <andreapoten3@gmail.com>
2024-11-19 18:38:18 +0100

    modificato

--- routers/posts.js
+++ routers/posts.js
@@ -5,37 +5,37 @@ const router = express.Router()
 // Index
 
 router.get('/posts', (req, res) => {
-    console.log('Lista dei post')
+    res.send('Lista dei post')
 })
 
 // Show
 
 router.get('/posts/id:', (req, res) => {
-    console.log(`Ecco il post con id ${req.params.id}`)
+    res.send(`Ecco il post con id ${req.params.id}`)
 })
 
 // Create
 
 router.post('/posts', (req, res) => {
-    console.log('Creazione del post')
+    res.send('Creazione del post')
 })
 
 // Update
 
 router.put('/posts/id:', (req, res) => {
-    console.log(`Aggiornato il post con id ${req.params.id}`)
+    res.send(`Aggiornato il post con id ${req.params.id}`)
 })
 
 // Modify
 
 router.patch('/posts/id:', (req, res) => {
-    console.log(`Modificato il post con id ${req.params.id}`)
+    res.send(`Modificato il post con id ${req.params.id}`)
 })
 
 // Delete
 
 router.delete('/posts/id:', (req, res) => {
-    console.log(`Cancellazione post con id ${req.params.id}`)
+    res.send(`Cancellazione post con id ${req.params.id}`)
 })
 
 module.exports = router
